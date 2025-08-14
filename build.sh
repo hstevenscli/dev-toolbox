96e58f9724faf7b27626751b6caed1178229c3b8
hstevenscli <16stevehun98@gmail.com>
2025-08-13 22:34:19 -0600

    build and toml setup

---
+++ build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# build landing page
+cd client/ && test -f package.json && npm run build; cd ..;
+
+# build each tool
+for dir in tools/*; do
+    cd "$dir" && test -f package.json && npm run build; cd -;
+done
+
+# build go server
+cd server/ && go build -o app;
