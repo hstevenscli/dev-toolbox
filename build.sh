c8de6f15c63b765e0f074fc91924a0283d25c1a7
hstevenscli <16stevehun98@gmail.com>
2025-08-16 13:55:59 -0600

    config nixpacks

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 # build landing page
-cd client/ && test -f package.json && npm run build; cd ..;
+cd client/ && test -f package.json && npm ci && npm run build; cd ..;
 
 # build each tool
 for dir in tools/*; do
-    cd "$dir" && test -f package.json && npm run build; cd -;
+    cd "$dir" && test -f package.json && npm ci && npm run build; cd -;
 done
 
 # build go server
-cd server/ && go build -o out  ../cmd/app;
+cd server/ && go build -o ../app;
